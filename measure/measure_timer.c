8791e625012732028acbaab2c03801c1adbe67fc
fsrmmomo <1906994189@qq.com>
2022-01-12 16:35:01 +0800

    修改日志

--- measure/measure_timer.c
+++ measure/measure_timer.c
@@ -96,18 +96,20 @@ void* print_current_time(void* argv){
 
 
     while(1){ 
-        printf("\nmeasurement module is running, statistics will be saved in /measure_log/statistics_log.txt\n");
+        
         count += 1;
         sleep(time_val);
         time(&now);
         timenow = gmtime(&now);
         //睡眠
+        printf("\nmeasurement module is running, statistics will be saved in /measure_log/statistics_log.txt\n");
         pthread_mutex_lock(send_mutex);  
         save_flow_statistics(count, send_sketch, send_Set, conn_ptr, 0);
+        save_flow_statistics(count, recv_sketch, recv_Set, conn_ptr, 1);
         pthread_mutex_unlock(send_mutex);
 
         pthread_mutex_lock(recv_mutex);  
-        save_flow_statistics(count, recv_sketch, recv_Set, conn_ptr, 1);
+        // save_flow_statistics(count, recv_sketch, recv_Set, conn_ptr, 1);
         pthread_mutex_unlock(recv_mutex);
         //printf("have save once");
     }
